22e86cfb23bd32963a60680aa5d42ba66416fb56
Sara Ismial <sara-ismial@outlook.com>
2026-01-17 17:28:02 -0600

    add project files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="min-h-screen bg-red-500 text-white flex items-center justify-center">
+        Tailwind test
+      </div>
+    </>
+  );
+}
+
+export default App;
